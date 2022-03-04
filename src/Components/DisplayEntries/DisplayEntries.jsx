7d09014ca177086b1e6d7a3205f0e3cfce8e94f4
Adam Frisch <afrisch88@outlook.com>
2022-03-04 16:52:01 -0600

    starting to code with react and start effecting elements on the browser

---
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -0,0 +1,28 @@
+
+const DisplayEntries = (props) => {
+    return ( 
+        <table>
+            <thead>
+            <tr>
+                <th>Entry Number</th>
+                <th>weight</th>
+                <th>date</th>
+            </tr>
+            </thead>
+            <tbody>
+            {props.parentEntries.map((entry, index) => {
+                return (
+                <tr>
+                    <td>{index+1}</td>
+                    <td>{entry.weight}</td>
+                    <td>{entry.date}</td>
+                </tr>
+                );
+            })}
+            </tbody>
+        </table>
+      
+     );
+}
+ 
+export default DisplayEntries;
